1e6120aafd9818d1b203ab6664776a077dfd1570
@luiz0067yahoo <102493028+luiz0067yahoo@users.noreply.github.com>
2026-08-12 10:48:27 -0300

    inicio

---
+++ wp-content/plugins/plugin_estudo/plugin_estudo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Plugin Estudo
+ * Plugin URI:        https://github.com/luiz0067yahoo/
+ * Description:       plugin backend de tema estudo.
+ * Version:           1.0
+ * Requires at least: 7.0
+ * Requires PHP:      8.0
+ * Author:            Luiz Fernando Brogliatto Ferreira
+ * Author URI:        https://github.com/luiz0067yahoo/
+ * License:           CopyRight Luiz Fernando
+ * Text Domain:       plugin-estudo
+ * Network:           True
+ *
+ */
+
