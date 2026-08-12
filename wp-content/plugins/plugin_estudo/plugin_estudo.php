36c46df5f8253c1c4efb12672a9df17e00202a96
@luiz0067yahoo <102493028+luiz0067yahoo@users.noreply.github.com>
2026-08-12 11:17:03 -0300

    start back-end

--- wp-content/plugins/plugin_estudo/plugin_estudo.php
+++ wp-content/plugins/plugin_estudo/plugin_estudo.php
@@ -14,3 +14,29 @@
  *
  */
 
+global $wpdb;//Váriavel global do wordpress para manipulação do banco de dados.
+
+define( 'WP_DEBUG', true );// Ativa o modo de depuração do WordPress
+define('WP_DEBUG_LOG', true);// Ativa o registro de erros em um arquivo de log
+define('WP_DEBUG_DISPLAY', true);// Exibe os erros na tela
+@ini_set('display_errors', 1);// Exibe os erros na tela
+
+ini_set('display_errors', 1);// Exibe os erros na tela
+ini_set('display_startup_errors', 1);// Exibe os erros de inicialização na tela
+error_reporting(E_ALL & ~(E_WARNING | E_DEPRECATED));// Exibe todos os erros, exceto avisos e mensagens deprecatadas
+
+define("PLUGIN_FILE_URL", __FILE__);// Define a constante PLUGIN_FILE_URL com o caminho do arquivo do plugin
+
+date_default_timezone_set('America/Sao_Paulo');// Define o fuso horário padrão para São Paulo
+
+add_action('init', function () {
+    header("Access-Control-Allow-Origin: *");// Permite que qualquer origem acesse os recursos do plugin
+    header("Access-Control-Allow-Methods: GET, OPTIONS");// Permite apenas os métodos GET e OPTIONS
+    header("Access-Control-Allow-Headers: *");// Permite que qualquer cabeçalho seja enviado na requisição
+});
+
+//require_once(plugin_dir_path(__FILE__) . '.env.php');//variável de ambiente do plugin
+//require_once(plugin_dir_path(__FILE__) . 'includes/config.php');//configurações do plugin
+//require_once(plugin_dir_path(__FILE__) . 'includes/db/_migrate.php');//migrar banco de dados do plugin
+//require_once(plugin_dir_path(__FILE__) . 'includes/controllers/_register_rest_route.php');//registrar rotas do plugin
+$wpdb->flush();
